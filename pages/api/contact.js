56747f756837d5c66a9738dfdef575df40f8a050
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-08-26 19:38:07 +0530

    Initial commit

---
+++ pages/api/contact.js
@@ -0,0 +1,32 @@
+import { supabase } from "@/lib/supabase";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST")
+    return res.status(405).json({
+      success: false,
+      message: "Please make a POST request",
+    });
+
+  const contactData = JSON.parse(req.body);
+
+  //   const contactData = {
+  //     fullName: "chamod",
+  //     email: "test@email.com",
+  //     subject: "Test email subject",
+  //     message: "Dummy email body",
+  //   };
+
+  const { error } = await supabase.from("contacts").insert([contactData]);
+
+  if (error)
+    return res.status(500).json({
+      success: false,
+      message: "Could not send your message. Please try again",
+    });
+
+  // Success message
+  res.status(200).json({
+    success: true,
+    message: "Thanks for your message! We will be in touch soon :)",
+  });
+}
